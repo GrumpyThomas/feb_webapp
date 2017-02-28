22905b8a0c9506276d933a248d62d1c8fc262491
McLaren <artyudin@outlook.com>
2017-02-27 20:48:35 -0500

    templates2

--- project/project/settings.py
+++ project/project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'nub9vd%63%^s8gu5-o&-faknphp_r(l2owm(twrnm&z(#qo6ao'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -120,10 +120,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
-
-STATIC_ROOT = '/static/'
+STATIC_ROOT = '/var/www/venv/feb_webapp/project/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static")
+    os.path.join(BASE_DIR, "/var/www/venv/feb_webapp/project/staticfiles")
 ]
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, '/var/www/venv/feb_webapp/project/media/')
